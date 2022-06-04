b27c4fe650eb2ebdb2e8c261a3ae66d3bfa75c89
YurySkosyrev <skosyrev-87@mail.ru>
2022-06-04 23:00:51 +0300

    Step 86/160

--- city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -11,7 +11,18 @@ import java.sql.SQLException;
 
 public class PersonCheckDao {
 
-    private static final String SQL_REQUEST = "";
+    private static final String SQL_REQUEST = "SELECT temporal FROM cr_address_person ap" +
+            "INNER JOIN cr_person p on p.person_id = ap.person_id" +
+            "INNER JOIN cr_address a on a.address_id = ap.address_id" +
+            "WHERE" +
+            "upper(p.sur_name) = upper(?) " +
+            "AND upper(p.given_name) = upper(?)" +
+            "AND upper(patronymic) = upper(?)" +
+            "AND p.date_of_birth = ?" +
+            "AND a.street_code = ?" +
+            "AND upper(a.building) = upper(?)" +
+            "AND upper(extension) = upper(?)" +
+            "AND upper(a.apartment) = upper(?)";
     public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
         PersonResponse response = new PersonResponse();
 
