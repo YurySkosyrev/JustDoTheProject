21831420d8f12e8f5a9f12af3d9ba6d539becde0
Aviaurez <skosyrev-87@mail.ru>
2022-06-10 22:57:49 +0300

    Step 104/160

---
+++ ext-system/city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
@@ -0,0 +1,21 @@
+package edu.javacourse.city.web;
+
+
+import edu.javacourse.city.domain.PersonResponse;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+
+
+@Path("/check")
+public class CheckPersonService
+{
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public PersonResponse checkPerson(@PathParam("id") int simpleId,
+                                      @QueryParam("name") String simpleName) {
+        return new PersonResponse();
+    }
+}
