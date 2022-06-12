d5bb70626afb766e1aa90517dcd165fd5d9fda17
YurySkosyrev <skosyrev-87@mail.ru>
2022-06-12 16:34:52 +0300

    Step 106/160

--- ext-system/city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
+++ ext-system/city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
@@ -1,9 +1,17 @@
 package edu.javacourse.city.web;
 
 
+import edu.javacourse.city.dao.PersonCheckDao;
+import edu.javacourse.city.dao.PoolConnectionBuilder;
 import edu.javacourse.city.domain.PersonRequest;
 import edu.javacourse.city.domain.PersonResponse;
+import edu.javacourse.city.exception.PersonCheckException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Singleton;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.awt.*;
@@ -11,13 +19,30 @@ import java.time.LocalDate;
 
 
 @Path("/check")
+@Singleton
 public class CheckPersonService
 {
+    private PersonCheckDao dao;
+    private static final Logger logger = LoggerFactory.getLogger(CheckPersonService.class);
+
+
+    @PostConstruct
+    public void init(){
+        logger.info("SERVLET is created");
+        dao = new PersonCheckDao();
+        dao.setConnectionBuilder(new PoolConnectionBuilder());
+    }
+
+    @PreDestroy
+    public void destroy(){
+        logger.info("FINISH");
+    }
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public PersonResponse checkPerson(PersonRequest request) {
-        System.out.println(request.toString());
-        return new PersonResponse();
+    public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
+        logger.info(request.toString());
+        return dao.checkPerson(request);
     }
 }
