05aae90c5df2d3bd5235bf64461028afcaf01506
YurySkosyrev <skosyrev-87@mail.ru>
2022-06-23 09:16:56 +0300

    Step 121/160

--- ext-system/register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
+++ ext-system/register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -1,13 +1,26 @@
 package edu.javacourse.register.business;
 
 import edu.javacourse.register.dao.MarriageDao;
+import edu.javacourse.register.domain.MarriageCertificate;
 import edu.javacourse.register.view.MarriageRequest;
 import edu.javacourse.register.view.MarriageResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MarriageManager {
-    private MarriageDao dao;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageManager.class);
+
+    private MarriageDao marriageDao;
+
+    public void setMarriageDao(MarriageDao marriageDao) {
+        this.marriageDao = marriageDao;
+    }
 
     public MarriageResponse findMarriageSertificate(MarriageRequest request){
-        throw new UnsupportedOperationException("Unsupported");
+        LOGGER.info("findMarriageSertificate called");
+        MarriageCertificate cert = marriageDao.findMarriageCertificate(request);
+
+        return new MarriageResponse();
     }
 }
